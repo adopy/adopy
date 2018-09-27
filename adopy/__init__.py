10b03b237f9e6f92c64c8f0a2033ff04fee29de8
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2018-09-27 21:16:07 +0900

    Restrict the module's namespace

--- adopy/__init__.py
+++ adopy/__init__.py
@@ -7,5 +7,12 @@ The package is based on the code by Woojae Kim and Bryan Zake.
 """
 from __future__ import absolute_import, division, print_function
 
-from .generics import ADOGeneric
+from . import functions
+from . import generic
+from . import task
 
+__all__ = [
+    'generic',
+    'functions',
+    'task'
+]
