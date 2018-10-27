5ba76dba7ea0c1865a8c9c7df4009e2f25826bff
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2018-10-28 05:40:49 +0900

    Rename as log_lik_bernoulli & Add log_lik_categorical

--- adopy/functions/_likelihood.py
+++ adopy/functions/_likelihood.py
@@ -4,11 +4,18 @@ from __future__ import print_function
 
 import numpy as np
 
-__all__ = ['log_lik_bern']
+__all__ = ['log_lik_bernoulli', 'log_lik_categorical']
 
 EPS = np.finfo(np.float).eps
 
 
-def log_lik_bern(y, p):
+def log_lik_bernoulli(y, p):
     """Log likelihood for Bernoulli random variable"""
     return y * np.log(p + EPS) + (1 - y) * np.log(1 - p + EPS)
+
+
+def log_lik_categorical(ys, ps):
+    ret = 0.
+    for y, p in zip(ys, ps):
+        ret += y * np.log(p + EPS)
+    return ret
