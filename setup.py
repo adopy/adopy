c54260ada154558b824f6f248d0d4c10ff325fdd
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2018-10-30 20:25:36 +0900

    Add flake8 for tests

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(name='adopy',
               'yapf',
           ],
           'test': [
+              'flake8',
               'codecov',
               'pytest',
               'pytest-cov',
