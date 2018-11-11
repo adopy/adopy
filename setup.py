dea465e22290714366fddc3544f685b3fd845517
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2018-11-11 16:09:21 +0900

    Update version to v0.1.1

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from __future__ import unicode_literals
 from codecs import open as codecs_open
 from setuptools import setup
 
+import adopy
+
 # Get the long description from the relevant file
 with codecs_open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='adopy',
-    version='0.1.0',
+    version=adopy.__version__,
     description='',
     long_description=long_description,
     classifiers=[],
