e11eed086d47c4ebb2dc9eaeb93e450baf014bcf
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-02-15 17:15:29 +0900

    Ignore overflow & underflow errors

--- adopy/__init__.py
+++ adopy/__init__.py
@@ -2,6 +2,7 @@
 ADOpy: Adaptive Design Optimization on Experimental Tasks
 """
 import os
+import numpy as np
 
 from adopy import base
 from adopy import functions
@@ -13,3 +14,6 @@ __all__ = ['base', 'functions', 'tasks', 'Task', 'Model', 'Engine']
 
 with open(os.path.join(os.path.dirname(__file__), 'VERSION'), 'r') as f:
     __version__ = f.read().strip()
+
+# Ignore overflow and underflow floating-point errors
+np.seterr(over='ignore', under='ignore')
