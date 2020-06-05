ac5010a11bd1ee9eb3360155273ecc453422ee58
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2020-06-05 22:30:54 +0900

    Use a custom-defined function for logsumexp

--- adopy/functions/_math.py
+++ adopy/functions/_math.py
@@ -2,9 +2,13 @@ from typing import Union
 
 import numpy as np
 
-__all__ = ['inv_logit']
+__all__ = ['inv_logit', 'logsumexp']
 
 
 def inv_logit(x: Union[float, np.ndarray]):
     """Calculate the inverse logit value of a given number :math:`x`."""
     return np.divide(1, 1 + np.exp(-x))
+
+
+def logsumexp(x: np.ndarray, axis: int = 0):
+    return np.log(np.sum(np.exp(x), axis=axis))
