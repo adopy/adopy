e075f3d85eea9542458a813c443181f3cbca21bc
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2020-11-27 17:07:04 +0900

    Use version from pyproject.toml

--- adopy/__init__.py
+++ adopy/__init__.py
@@ -8,8 +8,6 @@ import numpy as np
 from adopy import base, functions, tasks
 from adopy.base import Engine, Model, Task
 
-__version__ = '0.4.0.rc2'
-
 __all__ = [
     # Submodules
     'base', 'functions', 'tasks',
