015d53513a50a5accf10b97d00a3afd4173b989a
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2020-11-27 17:31:28 +0900

    Include __version__ on __init__.py

--- adopy/__init__.py
+++ adopy/__init__.py
@@ -8,6 +8,8 @@ import numpy as np
 from adopy import base, functions, tasks
 from adopy.base import Engine, Model, Task
 
+__version__ = '0.4.0-rc.3'
+
 __all__ = [
     # Submodules
     'base', 'functions', 'tasks',
